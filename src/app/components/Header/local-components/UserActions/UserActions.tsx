7407e3842303af0efb7fbd16c990fdb2bbd57b10
MoisesAzevedo <moisesdev2022@gmail.com>
2025-06-11 01:48:01 -0300

    feat: add user Actions

--- src/app/components/Header/local-components/UserActions/UserActions.tsx
+++ src/app/components/Header/local-components/UserActions/UserActions.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./UserActions.module.scss";
 import { getAssetPath } from "@/utils/paths";
 import { Tooltip } from "react-tooltip";
