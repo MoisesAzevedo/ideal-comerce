086f68a6fc6ddfb1948afabb9aad5fb321666f13
MoisesAzevedo <moisesdev2022@gmail.com>
2025-06-25 22:06:16 -0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isGithubPages ? '/ideal-comerce' : '',
-  assetPrefix: isGithubPages ? '/ideal-comerce/' : '',
+  output: "export",
+  basePath: isGithubPages ? "/ideal-comerce" : "",
+  assetPrefix: isGithubPages ? "/ideal-comerce/" : "",
   trailingSlash: true,
   images: {
     unoptimized: true
