d6b995823d69d7265e28e23323cf40c5644ec303
MoisesAzevedo <moisesdev2022@gmail.com>
2025-06-25 21:54:58 -0300

    feat: home page

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  basePath: isGithubPages ? '/ideal-comerce' : '',
+  assetPrefix: isGithubPages ? '/ideal-comerce/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  // Only apply basePath and assetPrefix for GitHub Pages deployment
-  ...(process.env.NODE_ENV === "production" &&
-    process.env.GITHUB_ACTIONS && {
-      basePath: "/ideal-comerce",
-      assetPrefix: "/ideal-comerce/"
-    }),
   distDir: "out"
 };
 
